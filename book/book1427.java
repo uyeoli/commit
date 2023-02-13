440a294d8ae826b864b0f177505ab5601b18f76e
uyeoli <uyeol134@naver.com>
2023-02-13 21:22:23 +0900

    10일차

---
+++ book/book1427.java
@@ -0,0 +1,18 @@
+package book;
+import java.util.Scanner;
+import java.util.Arrays;
+public class book1427 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] arr = sc.nextLine().toCharArray();
+		
+		Arrays.sort(arr);
+		
+		for(int i = arr.length - 1; i >=0 ; i--) {
+			System.out.print(arr[i]);
+		}
+		
+	}
+
+}
