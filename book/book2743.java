dc32d2fbd87468e977d172b670f23f316bc585f1
uyeoli <uyeol134@naver.com>
2023-02-27 20:36:28 +0900

    백준 : 단어 길이 재기

---
+++ book/book2743.java
@@ -0,0 +1,11 @@
+package book;
+import java.util.Scanner;
+public class book2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String st = sc.next();
+		System.out.println(st.length());
+	}
+
+}
